5f54949cd711825e080ef10670c3d0c9525bd09c
didierDefrance <didier@d-defrance.fr>
2021-11-17 15:53:40 +0100

    dessin

---
+++ tp/tp-js/jsWeb/js/dessin_jq.js
@@ -0,0 +1,19 @@
+var x1,y1,x2,y2;
+
+$( function(){
+    console.log("ok");
+
+    $("#mycanvas").on("mousedown" , (evt)=>{
+       //mémoriser les coordonnées x1,y1
+    })
+
+    $("#mycanvas").on("mouseup" , (evt)=>{
+         //mémoriser les coordonnées x2,y2
+         //on dessine une ligne de x1,y1 vers x2,y2
+    })
+
+    $("#mycanvas").on("mousemove" , (evt)=>{
+        //on affiche dans spanX et spanY les coordonnées evt...
+    })
+
+})
